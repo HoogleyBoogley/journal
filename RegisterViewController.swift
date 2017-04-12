3c61b67f3e01d97da7ca7e4933c0ca116d090f8b
Danny Tran <hoogleyboogley@minefieldmc.net>
2017-04-12 16:28:20 -0500

    Added Swift files

---
+++ RegisterViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RegisterViewController.swift
+//  HoogleyBoogley Journal
+//
+//  Created by Danny Tran on 4/10/17.
+//  Copyright © 2017 Danny Tran. All rights reserved.
+//
+
+import UIKit
+
+
+
+class RegisterViewController: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var firstName_field: UITextField!
+    @IBOutlet weak var lastName_field: UITextField!
+    @IBOutlet weak var username_field: UITextField!
+    @IBOutlet weak var email_field: UITextField!
+    @IBOutlet weak var password_field: UITextField!
+    @IBOutlet weak var pin_field: UITextField!
+    
+    
+    // Actions
+    @IBAction func register_button(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
